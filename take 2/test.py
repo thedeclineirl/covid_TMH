f2b6d528bfeb407d19c76a5ff9a0dfc80b3100c9
Thomas M. Higgins <61065565+thedeclineirl@users.noreply.github.com>
2022-05-17 00:17:54 +0100

    Update test.py
    Have it to a stage where I get a list of dict for each date
    
    - clean up
    -- added getJsonAsListofDict()
    -- removed some redundant code

--- take 2/test.py
+++ take 2/test.py
@@ -10,37 +10,46 @@ from urllib.request import urlopen
 import pandas as pd 
 import json
 
+
+##################################
+###	Functions
+##################################
+
 def listStripper(data,text):
 	result = []
 	for x in data:
 		result.append(x[text])
 	return result
 
+# Returns a list of Dictionary objects from the JSON retrieved from the given url
+def getJsonAsListofDict(url):
+	data = json.loads(urlopen(url).read())['features'] # Get the features to get a list of object
+	return(listStripper(data,'properties')) 
 
+##################################
+###	Variables
+##################################
 url = "https://services1.arcgis.com/eNO7HHeQ3rUcBllm/arcgis/rest/services/CovidStatisticsProfileHPSCIrelandOpenData/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
-response = urlopen(url)
-data = json.loads(response.read())
-data2 = data['features'] # Get the features to get a list of object
-# data3 = data2[-1]
-# data4 = data3['properties']
-
-data5 = listStripper(data2,'properties')
-print(len(data5))
-print(data5[-2],data5[-3],data5[-4],data5[-5])
 
 
-# df = pd.read_json(data,orient ='index')
-# df = pd.DataFrame.from_dict(data,orient="index")
-# y = json.dumps(data)
-# print(y)
 
-# print(data3.keys())
-# print(data['type'])
-# print(data4)
+##################################
+###	Sandbox
+##################################
+
+data = getJsonAsListofDict(url)
+
+data4 = data.pop()
+# print(data4.keys())
+data5 = data4.keys()
+for a in data4:
+	print(a.key())
+	print(a.value())
 
 
-#########
-# print(df.info())
+##################################
+###	NOTES
+##################################
 
+# Drop last 3 zeros from the json date to get the correct date in unix timestamp format
 
-# Drop last 3 zeros from the json date to get the correct date in unix timestamp
